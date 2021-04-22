ec6341be3c7d5f6ed7d09fadd3264d09e0459335
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-04-22 11:40:38 +0530

    Update Product_of_array_except_self.py

--- 238. Product of Array Except Self/Product_of_array_except_self.py
+++ 238. Product of Array Except Self/Product_of_array_except_self.py
@@ -3,7 +3,12 @@
 # the element itself. Now we make a result array of same length 
 # as nums array and initialize all elements with 1's. Let iterate over 
 # range of length of nums array, now iterate the result array from forward 
-# and backward simultaneously 
+# and backward simultaneously multiplied by value of p and q respectively
+# to find the final result array. Initial value of p and q is 1,and to find
+# all other values, we have to iterate nums array both forward and backward
+# simultaneously with consecutive multiplication of their elements from both 
+# and store it in p and q respectively till the loop ends. The final result array 
+# we get when all iterations over is the required answer. 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
