712a97143504bbfdf2db7a66e1d85dcfd918388e
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-04-22 09:33:05 +0530

    Update Minimum_Cost_to_Hire_K_Workers.py

--- Minimum Cost to Hire K Workers/Minimum_Cost_to_Hire_K_Workers.py
+++ Minimum Cost to Hire K Workers/Minimum_Cost_to_Hire_K_Workers.py
@@ -1,3 +1,32 @@
+#Approach:We are given n worker with their wage 
+and quality respectively, and we have to find
+k workers where each worker have their wages in
+ratio with quality and they should be paid 
+minimum wage exceptation as mention in wages.
+Let take an array with wage/quality ratio and 
+respective quality and sort the array for n 
+workers in accordance with wage/quality ratio. 
+Now we take a loop to iterate over first k 
+group of workers and push first k group of 
+workers quality with negative sign in the 
+minimum heap (first element every time 
+smallest,rest elements push and pop) and find
+total quality.Now calculate for first k group 
+of workers total wages = maximum wage/quality 
+ratio * total quality (sum of quality of first
+k workers).Now iterate over next k group of 
+workers and so on till we reach all quality 
+values and push their quality values in heap 
+and delete the quality value of first K-1 
+workers from heap to calculate the quality 
+value of ongoing k workers and multiplied it 
+with maximum wage/quality ratio (total quality
+= max(wage/quality) ratio * total quality).
+Now consider the minimum total wages out of 
+two total wages we calculate to find least 
+amount of money needed to form a paid group 
+satisfying the above conditions.
+
 class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], K: int) -> float:
         qr = []
