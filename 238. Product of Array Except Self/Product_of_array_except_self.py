a361d5d1a8ad2efd90cb8524d95d96fc75dcdf98
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-04-22 11:08:00 +0530

    Update Product_of_array_except_self.py

--- 238. Product of Array Except Self/Product_of_array_except_self.py
+++ 238. Product of Array Except Self/Product_of_array_except_self.py
@@ -1,3 +1,10 @@
+# Approach:We are given an array nums and we have to find result array 
+# where each element has product of all elements of nums array except 
+# the element itself. Now we make a result array of same length 
+# as nums array and initialize all elements with 1's. Let iterate over 
+range of length of nums array, now iterate the result array from forward 
+# and backward simultaneously 
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         p,q=1,1
