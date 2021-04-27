ae3184c2c6631aecc9ebf87ac8cee9a5f4ed3335
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-04-27 23:02:55 +0530

    Update Most-Stones-Removed-with-Same-Row-or-Column.py

--- 947. Most Stones Removed with Same Row or Column/Most-Stones-Removed-with-Same-Row-or-Column.py
+++ 947. Most Stones Removed with Same Row or Column/Most-Stones-Removed-with-Same-Row-or-Column.py
@@ -1,3 +1,15 @@
+# Approach:We are given a 2D array showing coordinates of stones in a 2D plane.
+# We have to remove stones from 2D plane if it shares row or column with any other stone,
+# and we have to find largest possible number of stones that can be removed with above condition.
+# To solve this, we have to create a dictionary and iterate over location of each stone and calculate 
+# union find(a) of each stone with row or column (to check stone can be removed with this 
+# row or column or not) and store it as a key value pair for each stone.
+# Here union find used for 2 things: 
+# a) To determine which group a particular element belongs to.
+# b) Find no of groups that has no effect on each other if element of one group is changed or removed.
+# Now we take union find of all elements of dictionary to calculate union find(b) groups.
+# Now to find largest no of stones that can be removed=Total no of stones - union find(b) groups.
+
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         uf = {}
